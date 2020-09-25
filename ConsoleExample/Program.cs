342350ca511522c54e8bdb9ea89f394a6e6c9f19
Craig Chamberlain <craig.chamberlain11@gmail.com>
2020-09-25 09:27:08 +0100

    Add simple console app to illustrate difference

---
+++ ConsoleExample/Program.cs
@@ -0,0 +1,41 @@
+﻿using Amazon.Lambda.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Amazon.DynamoDBv2;
+using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace LambdaExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   var _serviceProvider = StartUp.Container.BuildServiceProvider();
+            var DBClient = _serviceProvider.GetRequiredService<IAmazonDynamoDB>();
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+
+    public class StartUp
+    {
+        public static IServiceCollection Container => ConfigureServices();
+        public static IConfigurationRoot Configuration  => new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .Build();
+
+        private static IServiceCollection ConfigureServices()
+        {   
+            var services = new ServiceCollection();
+            
+            // This line is required but perhaps it shouldn't be?
+            services.AddDefaultAWSOptions(Configuration.GetAWSOptions());
+
+            services.AddAWSService<IAmazonDynamoDB>();
+                    
+            return services;
+        }
+    }
+    
+}
