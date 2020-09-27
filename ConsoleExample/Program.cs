fc586a8db1d2a8d4bea2f872ef9f6e21ccfd8122
Craig Chamberlain <48378435+CraigChamberlain@users.noreply.github.com>
2020-09-27 17:08:33 +0100

    Add environmental variables to configuration

--- ConsoleExample/Program.cs
+++ ConsoleExample/Program.cs
@@ -24,6 +24,7 @@ namespace LambdaExample
         public static IConfigurationRoot Configuration  => new ConfigurationBuilder()
         .SetBasePath(Directory.GetCurrentDirectory())
         .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables()
         .Build();
 
         private static IServiceCollection ConfigureServices()
